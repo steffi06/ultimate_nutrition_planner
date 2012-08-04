5b1b47ad63d0c6e70e26f88d8aea2f614dbc5a2e
Stephanie Shupe <shupe.stephanie@gmail.com>
2012-08-03 23:17:17 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UltimateNutritionPlanner::Application.config.secret_token = 'ce1dc3c20391e70ac53cd10f4c72f3c19514a34e1eaee979f78598f06ba25f3c91f27974f4f97eb85975e75e934e29295e12e83e2ba78e694f5d94d063767eba'
